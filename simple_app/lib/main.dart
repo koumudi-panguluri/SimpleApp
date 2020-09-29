fa5b7293bb0f2eb32bbf49640e49e2684e21cad5
purnimasaikoumudi <pangulurikoumudi@gmail.com>
2020-09-30 00:41:58 +0530

    app changes.

--- simple_app/lib/main.dart
+++ simple_app/lib/main.dart
@@ -1,117 +1,135 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import './transcation.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
+  final List<Transaction> transactions = [
+    Transaction(
+        title: 'First Expense',
+        id: 1,
+        price: 10,
+        currency: '\$',
+        date: DateTime.now(),
+        color: 0xff81D4FA),
+    Transaction(
+        title: 'Second Expense',
+        id: 2,
+        price: 20,
+        currency: '\$',
+        date: DateTime.now(),
+        color: 0xff9FA8DA)
+  ];
+  // String titleInput;
+  // String amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Koumi App'),
         ),
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                child: Card(
+                  child: Text('Main Card'),
+                  elevation: 10,
+                ),
+              ),
+              Column(
+                  children: transactions.map((tx) {
+                return Card(
+                  child: Row(children: [
+                    Container(
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Colors.white, style: BorderStyle.solid)),
+                      child: Text(
+                        tx.price.toString() + tx.currency,
+                        style: TextStyle(
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      padding: EdgeInsets.all(15),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(5),
+                          child: Text(
+                            'Name : ' + tx.title,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Container(
+                            padding: EdgeInsets.all(5),
+                            child: Text(
+                                'Date : ' + DateFormat.yMMMd().format(tx.date),
+                                style: TextStyle(color: Colors.black54))),
+                      ],
+                    )
+                  ]),
+                  elevation: 10,
+                  color: Color(tx.color),
+                );
+              }).toList()),
+              Card(
+                  color: Colors.blueGrey[50],
+                  elevation: 15,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Container(
+                            padding: EdgeInsets.all(15),
+                            child: Text(
+                              'Enter the Expenses',
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black54),
+                              textAlign: TextAlign.center,
+                            )),
+                        Container(
+                          padding: EdgeInsets.all(15),
+                          child: TextField(
+                            decoration: InputDecoration(labelText: 'Title'),
+                            controller: titleController,
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(15),
+                          child: TextField(
+                            decoration: InputDecoration(labelText: 'Amount'),
+                            controller: amountController,
+                          ),
+                        ),
+                        Container(
+                            child: FlatButton(
+                                child: Text(
+                                  'Add Transaction',
+                                  style: TextStyle(color: Colors.blueAccent),
+                                ),
+                                onPressed: () {
+                                  print(titleController.text);
+                                  print(amountController.text);
+                                })),
+                      ])),
+            ]),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ simple_app/lib/transcation.dart
@@ -0,0 +1,10 @@
+class Transaction {
+  final String title;
+  final int id;
+  final double price;
+  final String currency;
+  final DateTime date;
+  final int color;
+  Transaction(
+      {this.title, this.id, this.price, this.currency, this.date, this.color});
+}
